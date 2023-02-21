9bd15ed9bff6db0fed5a6d1d377b5b49a7a9bc7c
Megan Walker <mwalker@TEC0661DEAPP20.local>
2023-02-21 16:11:31 -0600

    site.css not loading in github pages fix

--- headers.js
+++ headers.js
@@ -20,7 +20,7 @@ class generateHeader extends HTMLElement {
     <title>My Personal Portfolio</title>
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link rel="stylesheet" href="../site.css" type="text/css">
+    <link rel="stylesheet" href="styles/site.css" type="text/css">
     `;
   }
 }
