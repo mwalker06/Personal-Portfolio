c3b274c6e4d75d337d58b4a22266fb3231a30308
Megan Walker <mwalker@TEC0661DEAPP20.local>
2023-06-09 15:54:45 -0500

    test to fix css or presentation pages

--- nav.js
+++ nav.js
@@ -14,7 +14,7 @@ class generateNav extends HTMLElement {
   // connect the component when called
   connectedCallback() {
     // the html to display when the tags are used
-    this.innerHTML = `<div class="sidenav"><a href="index.html">Home</a><a href="about.html">About</a><a href="resume.html">Resume</a><a href="projects.html">Projects</a><a href="database.html">Database Diagrams</a><a href="api.html">API Unit Tests</a><a href="bucketlist.html">Bucket List</a><a href="web-430/walker-tech-value-stream.html">Tech Value Stream Presentation</a><a href="web-430/walker-two-pizza-rule.html">Two-Pizza Rule Presentation</a><a href="web-430/walker-automated-tests.html">Automated Tests Presentation</a></div>
+    this.innerHTML = `<div class="sidenav"><a href="index.html">Home</a><a href="./about.html">About</a><a href="resume.html">Resume</a><a href="projects.html">Projects</a><a href="database.html">Database Diagrams</a><a href="api.html">API Unit Tests</a><a href="bucketlist.html">Bucket List</a><a href="web-430/walker-tech-value-stream.html">Tech Value Stream Presentation</a><a href="web-430/walker-two-pizza-rule.html">Two-Pizza Rule Presentation</a><a href="web-430/walker-automated-tests.html">Automated Tests Presentation</a></div>
     `;
   }
 }
